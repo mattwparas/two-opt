acc17732cf1be54985d49a6ff24ca611e834c5f3
frogbird <matthewparas2020@u.northwestern.edu>
2019-02-26 10:38:25 -0600

    formatted

--- two_opt.go
+++ two_opt.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 	"sync"
+	"time"
 )
 
 type Node struct {
@@ -59,8 +59,8 @@ func (t *TSP) swaps() {
 	bestObjective := t.calculate_objective()
 	tourLength := t.NumberOfNodes
 
-	for i := 0; i < tourLength; i++{
-		for j := 0; j < tourLength; j++{
+	for i := 0; i < tourLength; i++ {
+		for j := 0; j < tourLength; j++ {
 			node1 := t.Tour[i]
 			node2 := t.Tour[j]
 
@@ -69,9 +69,9 @@ func (t *TSP) swaps() {
 
 			sampleObjective := t.calculate_objective()
 
-			if sampleObjective < bestObjective{
+			if sampleObjective < bestObjective {
 				bestObjective = sampleObjective
-			} else{
+			} else {
 				t.Tour[i] = node1
 				t.Tour[j] = node2
 			}
@@ -80,14 +80,13 @@ func (t *TSP) swaps() {
 	}
 }
 
-
-func (t *TSP) performSwaps(){
+func (t *TSP) performSwaps() {
 	lastObjective := t.calculate_objective()
 	currentObjective := t.calculate_objective()
 	for {
 		t.swaps()
 		currentObjective = t.calculate_objective()
-		if currentObjective == lastObjective{
+		if currentObjective == lastObjective {
 			break
 		} else {
 			lastObjective = currentObjective
@@ -95,99 +94,80 @@ func (t *TSP) performSwaps(){
 	}
 }
 
-func (t *TSP) pprint(){
+func (t *TSP) pprint() {
 	nodeNumbers := make([]int, t.NumberOfNodes)
-	for i := 0; i < t.NumberOfNodes; i++{
+	for i := 0; i < t.NumberOfNodes; i++ {
 		nodeNumbers[i] = t.Tour[i].NodeNumber
 	}
 	fmt.Println(nodeNumbers)
 }
 
-func (t *TSP) shuffle(){
+func (t *TSP) shuffle() {
 	rand.Seed(time.Now().UnixNano())
 	newIndices := rand.Perm(t.NumberOfNodes)
 	newTour := make([]Node, t.NumberOfNodes)
-	for i:=0; i < t.NumberOfNodes; i++{
-		n:= Node{NodeNumber: i}
+	for i := 0; i < t.NumberOfNodes; i++ {
+		n := Node{NodeNumber: i}
 		newTour[newIndices[i]] = n
 	}
 	t.Tour = newTour
 }
 
-func (t *TSP) solve(){
+func (t *TSP) solve() {
 	t.shuffle()
 	t.performSwaps()
 }
 
-func buildTSPInstance(num int, distMat [][]int) TSP{
+func buildTSPInstance(num int, distMat [][]int) TSP {
 	nodeCount := num
 	newTour := make([]Node, nodeCount)
 
-	for i:=0; i < nodeCount; i++{
+	for i := 0; i < nodeCount; i++ {
 		n := Node{NodeNumber: i}
 		newTour[i] = n
 	}
 
 	// distMat := generate_matrix(nodeCount, 1000, true)
-	tspInstance := TSP{Tour: newTour, 
-					DistanceMatrix: distMat,
-					NumberOfNodes: nodeCount}
+	tspInstance := TSP{Tour: newTour,
+		DistanceMatrix: distMat,
+		NumberOfNodes:  nodeCount}
 
 	tspInstance.solve()
 
 	return tspInstance
 }
 
-
-
-
 func main() {
-
-	cityCount := 200
-
+	cityCount := 500
 	distMat := generate_matrix(cityCount, 1000, true)
-
 	start := time.Now()
-
-	randomRestartsRun := 200
-
+	randomRestartsRun := 10
 	solutions := make([]TSP, randomRestartsRun)
 
 	var wg sync.WaitGroup
-	for i:=0; i < randomRestartsRun; i++{
+	for i := 0; i < randomRestartsRun; i++ {
 		wg.Add(1)
-		go func(j int){
+		go func(j int) {
 			defer wg.Done()
 			output_tsp := buildTSPInstance(cityCount, distMat)
 			solutions[j] = output_tsp
-		} (i)
+		}(i)
 	}
 	wg.Wait()
 	// Find minimum
 
 	bestSolution := solutions[0]
 	bestObjective := bestSolution.calculate_objective()
-	for i:=0; i < randomRestartsRun; i++{
-		if solutions[i].calculate_objective() < bestObjective{
+	for i := 0; i < randomRestartsRun; i++ {
+		if solutions[i].calculate_objective() < bestObjective {
 			bestSolution = solutions[i]
 			bestObjective = bestSolution.calculate_objective()
 		}
 	}
 
-	fmt.Printf("Best Solution After %v Iterations: %v\n", randomRestartsRun, bestObjective)
-
+	fmt.Printf("Best Solution After %v Iterations: %v\n",
+		randomRestartsRun, bestObjective)
 	elapsed := time.Since(start)
-    fmt.Println("Two-Opt took: ", elapsed)
+	fmt.Println("Two-Opt took: ", elapsed)
 
 }
-
-
-
-
-
-
-
-
-
-
-
