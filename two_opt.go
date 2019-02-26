2f2308d884c18d0e6c9bbccc7db9101c0b5e4e2c
frogbird <matthewparas2020@u.northwestern.edu>
2019-02-26 10:08:00 -0600

    run multiple instances simultaneously

--- two_opt.go
+++ two_opt.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"sync"
 )
 
 type Node struct {
@@ -43,7 +44,7 @@ func generate_matrix(size int, maxDistance int, symmetric bool) [][]int {
 	}
 
 	if symmetric == true {
-		// Make matrix symmetric
+		// Make matrix symmetrick
 		for i := 0; i < size; i++ {
 			for j := 0; j < size; j++ {
 				grid[i][j] = grid[j][i]
@@ -83,7 +84,6 @@ func (t *TSP) swaps() {
 func (t *TSP) performSwaps(){
 	lastObjective := t.calculate_objective()
 	currentObjective := t.calculate_objective()
-
 	for {
 		t.swaps()
 		currentObjective = t.calculate_objective()
@@ -103,12 +103,24 @@ func (t *TSP) pprint(){
 	fmt.Println(nodeNumbers)
 }
 
-// func (t *TSP)
+func (t *TSP) shuffle(){
+	rand.Seed(time.Now().UnixNano())
+	newIndices := rand.Perm(t.NumberOfNodes)
+	newTour := make([]Node, t.NumberOfNodes)
+	for i:=0; i < t.NumberOfNodes; i++{
+		n:= Node{NodeNumber: i}
+		newTour[newIndices[i]] = n
+	}
+	t.Tour = newTour
+}
 
+func (t *TSP) solve(){
+	t.shuffle()
+	t.performSwaps()
+}
 
-func main() {
-
-	nodeCount := 500
+func buildTSPInstance(num int, distMat [][]int) TSP{
+	nodeCount := num
 	newTour := make([]Node, nodeCount)
 
 	for i:=0; i < nodeCount; i++{
@@ -116,29 +128,56 @@ func main() {
 		newTour[i] = n
 	}
 
-	list := rand.Perm(25)
-	for i, _ := range list {
-	    list[i]++
-	}
-	
-	fmt.Println(list)
-
-	distMat := generate_matrix(nodeCount, 1000, true)
-
+	// distMat := generate_matrix(nodeCount, 1000, true)
 	tspInstance := TSP{Tour: newTour, 
 					DistanceMatrix: distMat,
 					NumberOfNodes: nodeCount}
 
+	tspInstance.solve()
+
+	return tspInstance
+}
+
+
+
+
+func main() {
+
+	cityCount := 200
+
+	distMat := generate_matrix(cityCount, 1000, true)
+
 	start := time.Now()
 
-	fmt.Println("Initial Objective:", tspInstance.calculate_objective())
-	tspInstance.performSwaps()
+	randomRestartsRun := 200
 
-	fmt.Println("Final Objective:", tspInstance.calculate_objective())
+	solutions := make([]TSP, randomRestartsRun)
+
+	var wg sync.WaitGroup
+	for i:=0; i < randomRestartsRun; i++{
+		wg.Add(1)
+		go func(j int){
+			defer wg.Done()
+			output_tsp := buildTSPInstance(cityCount, distMat)
+			solutions[j] = output_tsp
+		} (i)
+	}
+	wg.Wait()
+	// Find minimum
+
+	bestSolution := solutions[0]
+	bestObjective := bestSolution.calculate_objective()
+	for i:=0; i < randomRestartsRun; i++{
+		if solutions[i].calculate_objective() < bestObjective{
+			bestSolution = solutions[i]
+			bestObjective = bestSolution.calculate_objective()
+		}
+	}
+
+	fmt.Printf("Best Solution After %v Iterations: %v\n", randomRestartsRun, bestObjective)
 
 	elapsed := time.Since(start)
-    fmt.Println("Binomial took: ", elapsed)
-	// tspInstance.pprint()
+    fmt.Println("Two-Opt took: ", elapsed)
 
 }
 
